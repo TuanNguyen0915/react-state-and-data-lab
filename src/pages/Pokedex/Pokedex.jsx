31d32b83a70f592b40880b43415f5be6c09426f0
Tuan Nguyen <tuan.nat915@gmail.com>
2023-10-05 09:30:55 -0400

    part 2-show the page based on currIdx and displayCount

--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -2,30 +2,52 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import "./Pokedex.css"
 import { pokeData } from "../../data/pokeData"
-console.log(pokeData)
+console.log(pokeData);
 
 
 const Pokedex = () => {
+
   const displayCount = 10
   const [currIdx, setCurrIdx] = useState(0)
-  const [displayedPokemon, setDisplayedPokemon] = useState(pokeData)
+  const [displayedPokemon, setDisplayedPokemon] = useState(filterPokemonData(0))
 
-  return ( 
+  
+  function filterPokemonData(newIdx) {
+    const res = pokeData.filter((poke, idx) => {
+      return idx >= newIdx && idx < newIdx + displayCount
+    })
+    console.log(res);
+    return res
+  }
+
+  const handleIncrease = () => {
+    let newIdx = currIdx + displayCount
+    setCurrIdx(newIdx)
+    setDisplayedPokemon(filterPokemonData(newIdx))
+  }
+
+  const handleDecrease = () => {
+    let newIdx = currIdx - displayCount
+    setCurrIdx(newIdx)
+    setDisplayedPokemon(filterPokemonData(newIdx))
+  }
+
+  return (
     <>
       <h1>Pokemon List</h1>
       <div className="pagination-container">
-        <button>&lt;</button>
-        <button>&gt;</button>
+        <button onClick={handleDecrease}>&lt;</button>
+        <button onClick={handleIncrease}>&gt;</button>
       </div>
       <div className="num-results-container">
-        Results ## - ## of {pokeData.length}
+        Results {currIdx + 1} - {currIdx + displayCount} of {pokeData.length}
       </div>
       <div className="pokemon-container">
-        {displayedPokemon.map(pokemon => 
+        {displayedPokemon.map(pokemon =>
           <Link to={`/pokemon/${pokemon.number - 1}`} key={pokemon._id}>
             <div className="pokemon-link">
               <img src={pokemon.image} alt="a fierce pokemon" />
-              <p>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>  
+              <p>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
             </div>
           </Link>
         )}
